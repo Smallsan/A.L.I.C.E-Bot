7f84caa6f643f1b2bcf208dacfe9dd96ec0bc3c7
Smallsan <126304322+Smallsan@users.noreply.github.com>
2023-07-21 11:57:51 +0800

    my first commitment

---
+++ aliceBot.js
@@ -0,0 +1,14 @@
+const { Client, Intents, GatewayIntentBits } = require('discord.js');
+const { logMessage } = require('./modules/messageLogger');
+const client = new Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds ] });
+const keys = require("./keys.json")
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+    if (message.author.bot || !message.guild) return;
+    logMessage(message);
+});
+
+client.login(keys.discordApi);
