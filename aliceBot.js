288ee93d5fd1ccaa5dd2f84324106b2e1a7e3eaa
Smallsan <126304322+Smallsan@users.noreply.github.com>
2023-07-22 13:55:20 +0800

    added moderation log feature

--- aliceBot.js
+++ aliceBot.js
@@ -1,5 +1,6 @@
 const { Client, Intents, GatewayIntentBits } = require('discord.js');
 const { logMessage } = require('./modules/messageLogger');
+const { logMessageToChannel } = require('./modules/MessageChannelLogger');
 const client = new Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds ] });
 const keys = require("./keys.json")
 client.on('ready', () => {
@@ -9,6 +10,10 @@ client.on('ready', () => {
 client.on('messageCreate', async (message) => {
     if (message.author.bot || !message.guild) return;
     logMessage(message);
+    logMessageToChannel(message, client);
+
+    
 });
 
 client.login(keys.discordApi);
+
