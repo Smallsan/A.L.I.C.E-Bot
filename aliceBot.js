55123bea6ebe4d3f83bd7a2d255b7e0a3e7f1254
Smallsan <126304322+Smallsan@users.noreply.github.com>
2023-07-23 12:44:27 +0800

    added custom Presence

--- aliceBot.js
+++ aliceBot.js
@@ -1,19 +1,25 @@
-const { Client, Intents, GatewayIntentBits } = require('discord.js');
-const { logMessage } = require('./modules/messageLogger');
-const { logMessageToChannel } = require('./modules/MessageChannelLogger');
-const client = new Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds ] });
-const keys = require("./keys.json")
+const { Client, Intents, GatewayIntentBits } = require('discord.js')
+const { logMessage } = require('./modules/messageLogger')
+const { logMessageToChannel } = require('./modules/MessageChannelLogger')
+const { setPresence } = require('./modules/Presence')
+const client = new Client({
+  intents: [
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.Guilds
+  ]
+})
+const keys = require('./keys.json')
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+  console.log(`Logged in as ${client.user.tag}`)
+  setPresence(client)
+})
 
-client.on('messageCreate', async (message) => {
-    if (message.author.bot || !message.guild) return;
-    logMessage(message);
-    logMessageToChannel(message, client);
+client.on('messageCreate', async message => {
+  if (message.author.bot || !message.guild) return
+  logMessage(message)
+  logMessageToChannel(message, client)
+})
 
-    
-});
-
-client.login(keys.discordApi);
-
+client.login(keys.discordApi)
---
+++ modules/Presence.js
@@ -0,0 +1,21 @@
+const config = require('../config/config.json')
+const { ActivityType } = require('discord.js')
+
+//capitalizes the first letter of the string for the ActivityType
+function capitalizeFirstLetter (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+}
+
+function setPresence (client) {
+  const presenceName = config.presenceName
+  const presenceType = capitalizeFirstLetter(config.presenceType)
+  const url = config.url
+
+  client.user.setActivity({
+    name: presenceName,
+    type: ActivityType[presenceType],
+    url: url
+  })
+}
+
+module.exports = { setPresence }
