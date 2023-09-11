2152cc17208167259f610e58c1bf8fc377888f7b
Smallsan <126304322+Smallsan@users.noreply.github.com>
2023-09-11 21:41:57 +0800

    Added fix for users with null displaynames

--- modules/LogEmbedFormatter.js
+++ modules/LogEmbedFormatter.js
@@ -1,26 +1,28 @@
-import { EmbedBuilder } from 'discord.js'
+import { EmbedBuilder } from 'discord.js';
 
-export function formatLogEmbed (message) {
-  const instant = new Date()
-  const user = message.author
-  const messageLink = `[**Jump To Message**](https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id})`
-  const channelLink = `[#${message.channel.name}](https://discord.com/channels/${message.guild.id}/${message.channel.id})`
+export function formatLogEmbed(message) {
+  const instant = new Date();
+  const user = message.author;
+  const messageLink = `[**Jump To Message**](https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id})`;
+  const channelLink = `[#${message.channel.name}](https://discord.com/channels/${message.guild.id}/${message.channel.id})`;
+
+  let displayName = message.member?.displayName || 'Null';
 
   const embed = new EmbedBuilder()
     .setColor('#0099FF')
     .setTitle(user.tag)
-    .setAuthor({ name: message.member.displayName, iconURL: user.avatarURL() })
+    .setAuthor({ name: displayName, iconURL: user.avatarURL() })
     .setDescription(
       `** Message sent in ** ${channelLink}\n${messageLink}\n${message.content}`
     )
     .setThumbnail(user.avatarURL())
     .setFooter({ text: `User ID: ${user.id}` })
-    .setTimestamp(instant)
+    .setTimestamp(instant);
+
   if (message.attachments.size > 0) {
-    const attachment = message.attachments.first()
-    embed.setImage(attachment.url)
+    const attachment = message.attachments.first();
+    embed.setImage(attachment.url);
   }
-  return embed
+
+  return embed;
 }
-
-
